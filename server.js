ae87470355a68f4ac2e5e12af6c87e3dcbadebb7
David Acuña <davic1987@gmail.com>
2026-02-25 15:52:52 -0600

    segundo commit

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const { createServer } = require('http')
-const next = require('next')
 const { parse } = require('url')
- 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const next = require('next')
+
+const dev = false
+const hostname = 'localhost'
+const port = process.env.PORT || 3000
+const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
- 
+
 app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res)
-  }).listen(port)
- 
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true)
+      await handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
 })
