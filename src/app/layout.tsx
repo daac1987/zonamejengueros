379b8326b0a924b5924a26b43924bdf43455f3b7
David Acuña <davic1987@gmail.com>
2026-03-08 15:01:57 -0600

    vercel speedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Footer from "@/src/components/Footer";
 import { Toaster } from "sonner";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights />
         <Toaster position="top-center" theme="dark" richColors />
         <Footer />
       </body>
